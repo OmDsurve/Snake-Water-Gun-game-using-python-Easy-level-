bb4fb85c636bf5ac8fe1bcb783aead628610ff4b
Om Surve <omdsurve2003@gmail.com>
2024-08-11 23:35:22 +0530

    Add files via upload

---
+++ 01_main.py
@@ -0,0 +1,27 @@
+import random
+
+computer = random.choice([-1,1,0])
+youstr = input("Enter Your choise: ")
+youDict = {"s":1,"w":-1,"g":0}
+reverseDict = {1 :" Snake",-1:" Water",0:" Gun"}
+you = youDict[youstr]
+
+print(f"You chose{reverseDict[you]}\nComputer chose{reverseDict[computer]}")
+
+if(computer==you):
+    print("It is a draw")
+else:
+    if(computer==1 and you==-1):
+        print("You lose!")
+    elif(computer==1 and you==0):
+        print("You win!") 
+    elif(computer==-1 and you==0):
+        print("You lose!")
+    elif(computer==-1 and you==1):
+        print("You win!")
+    elif(computer==0 and you==1):
+        print("You lose!")
+    elif(computer==0 and you==-1):
+        print("You win!")
+    else:
+        print("something went wrong")                           
